3c84ac07c0315862cddc42707826188ad98b6865
dotsivak <sivaksivakanthan@gmail.com>
2025-01-11 12:33:08 -0500

    feat: export Layer

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export { Frame } from "./components/Frame";
 export { Input } from "./components/Input";
 export { Border } from "./components/Border";
 export { Corner } from "./components/Corner";
+export { Layer } from "./components/Layer";
+export { Group } from "./components/Group";
 export { Gradient } from "./utils/gradient";
-export {};
